57ffd5ccd858b2ea4e1655d23dcb5013fbd7ee76
MireyaPrieto <miprios1990@gmail.com>
2023-06-05 11:17:49 -0500

    menu y fondo en html y css

--- src/data.js
+++ src/data.js
@@ -1,9 +1,5 @@
-// estas funciones son de ejemplo
+export const ghibli = () => {
+    return 'ghibli';
+  };
+  
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-console.log(example, data);
+import data from './data/ghibli/ghibli.js';
+import {ghibli} from './data.js';
+console.log( data.films);
+console.log( ghibli );
