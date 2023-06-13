34a6fc0db3c42e659dbadadf3d9c44a833258773
NayenLuna <nayenromancepeda@gmail.com>
2023-06-13 14:15:29 -0400

    edicion imagenes css

--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
-console.log(ghibli, data);
+
 import data from './data/ghibli/ghibli.js';
 import {ghibli} from './data.js';
-console.log( data.films);
-console.log( ghibli );
+const peliculas = data.films
+for (let i = 0; i < peliculas.length; i++) {
+    const element = peliculas[i];
+    
+    console.log( element.title);
+    console.log (element.director);
+    const contenedor = document.getElementById("contenedor");
+    for (let i = 0; i < peliculas.length; i++) {
+      let element = peliculas[i];
+      console.log(element.poster);
+      const img = document.createElement("img");
+      img.src = element.poster;
+      contenedor.appendChild(img);
+    }
+}
